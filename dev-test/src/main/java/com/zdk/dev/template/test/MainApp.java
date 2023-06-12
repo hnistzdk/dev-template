42c4546b5e9bd8186390c795edcd5be87776e311
hnistzdk <369365576@qq.com>
2023-06-12 19:31:37 +0800

    redis+redisson lock

---
+++ dev-test/src/main/java/com/zdk/dev/template/test/MainApp.java
@@ -0,0 +1,17 @@
+package com.zdk.dev.template.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author zdk
+ * @Date 2023/6/12 17:48
+ */
+
+@SpringBootApplication
+public class MainApp{
+    public static void main(String[] args){
+        SpringApplication.run(MainApp.class,args);
+    }
+}
